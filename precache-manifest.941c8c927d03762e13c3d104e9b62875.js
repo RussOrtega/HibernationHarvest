a709fe1dc9cd48501c4c816c84e7c6d055eb81bd
@RussOrtega <83038530+RussOrtega@users.noreply.github.com>
2021-05-23 10:35:52 -0500

    Updates

---
+++ precache-manifest.941c8c927d03762e13c3d104e9b62875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1184130a41597ba000bbdf445c0b8e82",
+    "url": "/HibernationHarvest/index.html"
+  },
+  {
+    "revision": "83df613c09daf5662b27",
+    "url": "/HibernationHarvest/static/css/main.43ac1b70.chunk.css"
+  },
+  {
+    "revision": "3b9ee0eac6b5b89abdaa",
+    "url": "/HibernationHarvest/static/js/2.bb50d5a6.chunk.js"
+  },
+  {
+    "revision": "09f27136d5c557341b6751b1cd5e2ee4",
+    "url": "/HibernationHarvest/static/js/2.bb50d5a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83df613c09daf5662b27",
+    "url": "/HibernationHarvest/static/js/main.98faee6c.chunk.js"
+  },
+  {
+    "revision": "80749e6c1a2fc0fe660b",
+    "url": "/HibernationHarvest/static/js/runtime-main.dbc6c567.js"
+  }
+]);
